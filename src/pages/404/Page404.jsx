09e9d911a8282c6131473f86fc3d8e952d4cbca8
PabloLimaSouza <pabloraboni@outlook.com>
2024-07-11 23:20:43 -0300

    Ajustes diversos

--- src/pages/404/Page404.jsx
+++ src/pages/404/Page404.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './Page404.module.scss';
+import React from "react";
+import styles from "./Page404.module.scss";
 
 const Page404 = () => {
   return (
-    <div className={styles["pr-page__404"]}>Erro 404. Página não encontrada!</div>
-  )
-}
+    <div className={styles["pr-page__404"]}> 
+      Erro 404. Página não encontrada!
+    </div>
+  );
+};
 
-export default Page404
+export default Page404;
